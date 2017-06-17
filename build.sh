902a8caeaa2f86c9b8671500649d26699d6ba609
Jonathan Campbell <jonathan@castus.tv>
2017-06-17 00:08:43 -0700

    more

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-./configure --enable-osmesa --enable-glx --enable-debug --enable-gpu-debug --disable-div-debug --disable-sqrt-debug --enable-dma-debug --prefix=/usr
+./configure --enable-osmesa --enable-glx --enable-debug --enable-gpu-debug --disable-div-debug --disable-sqrt-debug --enable-dma-debug --prefix=/usr || exit 1
+make -j5 || exit 1
 
